72fce3c2b25179b98338617cbbf54399c435b0b9
NeelDutt <indra@neeldutt.com>
2024-12-19 17:02:38 +0530

    C practice on 19 dec 24 @ 1703

--- farencel.c
+++ farencel.c
@@ -4,9 +4,9 @@ void main(){
     float celc;
     faren = 32;
     celc = 0.000000;
-    while (faren <= 50){
-        celc = (float)((faren-32)*5)/9;
-        printf ("%f\n", celc);
-        faren = faren + 1;
-    }
+    printf ("Please enter the temperature in Farenheit: ");
+    scanf("%d", &faren); // accept the farenheit value from user
+    celc=(float)(((faren-32)*5)/9); // convert the farenheit value to celcius. store via casting
+    printf ("The value is %f", celc); //print the celcius value
+    
 }
